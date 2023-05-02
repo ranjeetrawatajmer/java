d9bd24cd360486f28d68723113d3cd1f6f67f6f0
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-02 12:05:23 +0530

    hhhh

---
+++ new.java
@@ -0,0 +1,8 @@
+class Ranjeet
+{
+    public static void main(String arr[])
+    {
+        System.out.println("Name => ranjeet Rawat");
+        System.out.println("valage_Name => Narwar");
+    }
+}
