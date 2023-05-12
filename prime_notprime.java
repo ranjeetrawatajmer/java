00a86f331e5b6cb59add05e01694894703994c3c
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-12 11:49:26 +0530

    PRIME NOT NOT PRIME

---
+++ prime_notprime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Prime{
+    public static void main(String arr[]) {
+        int a,x=0,i=1;
+        Scanner ob = new Scanner(System.in);
+
+        System.out.print("Enter Number ");
+        a=ob.nextInt();
+
+        do{
+            if(a%i==0){
+                x++;
+            }
+            i++;
+        }while(i<=a);
+
+        if(x==2){
+            System.out.print("It is prime Number");
+        }
+        else
+        System.out.print("It is Not Prime Number");
+    }
+}
