bef8944be6f821b624cbca98dcc26b2cf3d02baa
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-15 11:38:45 +0530

    STRING LENGTH

---
+++ string_length.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class st
+{
+    public static void main(String arr[]) 
+    {
+      String st;
+      int len;
+
+      System.out.print("ENTER A STRING: ");
+      Scanner ob = new Scanner(System.in);
+      st= ob.next();
+      len= st.length();
+      System.out.println("length is= "+len);
+    }
+}
