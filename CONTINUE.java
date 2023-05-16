2d55a9443df0321494d924552f4134930286a073
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-16 10:24:32 +0530

    CONTINUE_STATMENT

---
+++ CONTINUE.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class cantinew
+{
+    public static void main(String arr[]) 
+    {
+    int i=1;
+    System.out.println("enter a number: ");
+    while(i<=4)
+    {
+      System.out.println(i);
+      if(i>=4)
+      {
+        i=1;
+        continue;
+      }
+      i++;
+     
+    }
+    }
+}
