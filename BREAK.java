b5db92fd1d9a980558fdc96a941531d907f0e9c7
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-16 10:15:16 +0530

    BREAK_STATMENT

---
+++ BREAK.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class br
+{
+    public static void main(String arr[]) 
+    {
+    int i=1;
+    System.out.println("enter a number: ");
+    while(i<=4)
+    {
+      System.out.println(i);
+      if(i>=2)
+    
+      break;
+      i++;
+     
+    }
+    }
+}
