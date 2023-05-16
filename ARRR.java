c84162b5fe33d857d896407e4dff6f31210bc192
ranjeetrawatajmer <ranjeetrawatsoftdev@gmail.com>
2023-05-16 10:48:10 +0530

    ARRAY_PROGRAM

---
+++ ARRR.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class arr
+{
+    public static void main(String arr[]) 
+    {
+     int a[];
+     a= new int[2];
+     int b[]= new int [2];
+     int c[]= new int []{33,44};
+     
+     System.out.println(a[0]);
+     System.out.println(a[1]);
+     
+     System.out.println();
+
+     System.out.println(b[0]);
+     System.out.println(b[1]);
+     
+     System.out.println();
+
+     System.out.println(c[0]);
+     System.out.println(c[1]);
+    }
+}
